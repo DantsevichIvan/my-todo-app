4b2e9152b145ee6bd56e24387a37ef3d6c49ef3b
DantsevichIvan <iw.dantsevich@gmail.com>
2024-11-02 11:17:47 +0100

    Initial commit

---
+++ functions.py
@@ -0,0 +1,24 @@
+import os
+FILEPATH = "todos.txt"
+
+if not os.path.exists(FILEPATH):
+    with open("todos.txt", "w") as file:
+        pass
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of
+    to-do items.
+    """
+
+    with open(filepath, "r") as file_local:
+        return file_local.readlines()
+
+
+def write_todos(value, filepath=FILEPATH):
+    """ Write the to-do items list in the text file."""
+    with open(filepath, "w") as file_local:
+        file_local.writelines(value)
+
+
+if __name__ == "__main__":
+    print(get_todos())
